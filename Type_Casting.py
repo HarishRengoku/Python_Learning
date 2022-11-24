d92a32e59545c83d7732cc51ffd955dcbb65990f
Harish Ganesh <94498641+HarishRengoku@users.noreply.github.com>
2022-11-24 19:13:03 +0530

    Add files via upload

---
+++ Type_Casting.py
@@ -0,0 +1,13 @@
+print("This program can be used to convert data types of strings, it is also called as type casting: ")
+
+x = 1 #int
+y = 2.3 #float
+z = "3" #string
+
+x = str(x)
+y = int(y)
+z = float(z)
+
+print("X is now ", x)
+print("Y is now ", y)
+print("Z is now ", z)
