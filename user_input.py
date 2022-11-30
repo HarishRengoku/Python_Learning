208bb8f1450b9b9033efa2adfab65a7ab03f2659
Harish Ganesh <94498641+HarishRengoku@users.noreply.github.com>
2022-11-30 18:12:40 +0530

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+print("Hello",name)
+
+age = int(input("What is your age? "))
+print("You are ",age," years old")
+print("After 5 years you will be ", age+5, "years old")
+
+h = float(input("How tall are you? "))
+print("Your height is ",h," cm ")
